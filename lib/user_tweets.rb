2bb2092220e1853bf52bdd1c770fe27770176bc6
César Mejía <ctkrecords@github.com>
2019-11-24 12:30:21 -0600

    Triying to fix the request to twitter API

--- lib/user_tweets.rb
+++ lib/user_tweets.rb
@@ -1,16 +1,16 @@
 module UserTweets
     def self.twitter_search(username)
         client = Twitter::REST::Client.new do |config|
-            config.consumer_key        = ENV.fetch("TWITTER_CONSUMER_KEY")
-            config.consumer_secret     = ENV.fetch("TWITTER_CONSUMER_SECRET")
-            config.access_token        = ENV.fetch("TWITTER_ACCESS_TOKEN")
-            config.access_token_secret = ENV.fetch("TWITTER_ACCESS_TOKEN_SECRET")
+            config.consumer_key        = "Ce1hUsinSHGerD9BL37CKU81p"#ENV.fetch("TWITTER_CONSUMER_KEY")
+            config.consumer_secret     = "c4zqxpjt4w2tvvchQWkw4rOhLT3ri4UBgaittiCFeHy1CXtLvJ"#ENV.fetch("TWITTER_CONSUMER_SECRET")
+            config.access_token        = "71112106-TpCpyll0ru4p05v9leqiHHZ9NWboxhtjtKqX45wpO"#ENV.fetch("TWITTER_ACCESS_TOKEN")
+            config.access_token_secret = "MOJk06Z4mColWVHCzFNsvZYQMuANCojVDyXZ9yyqiAt5u"#ENV.fetch("TWITTER_ACCESS_TOKEN_SECRET")
         end
 
         begin
             response = client.user_timeline(username, count: 200)
         rescue Twitter::Error => e
-            response = e
+            response = nil
         end
 
         if response.nil?
